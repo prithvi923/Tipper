d9e1b9c8075e1a1e35ea5a546d2f6d4b078d26f6
Prithvi Prabahar <prithvi923@gmail.com>
2016-09-11 21:11:28 -0400

    Animate text field area

---
+++ Tipper/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Tipper
+//
+//  Created by Prithvi Prabahar on 9/11/16.
+//  Copyright © 2016 Prithvi Prabahar. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    static let appGreenColor = UIColor(red: 120.0/255.0, green: 220.0/255.0, blue: 123.0/255.0, alpha: 1.0)
+}
--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -10,25 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var baseView: UIView!
     var baseField: UITextField!
     var tipLabel: UILabel!
     var totalLabel: UILabel!
     var tipControl: UISegmentedControl!
     var tipValues: [String] = ["15%", "20%", "25%"]
+    var keyboardHeight: CGFloat!
+    var baseViewConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow), name: UIKeyboardWillShowNotification, object: nil)
+        
+        createBaseView()
         createInputBaseField()
         createTipLabel()
         createTotalLabel()
         createTipControl()
     }
     
+    func createBaseView() {
+        baseView = UIView()
+        
+        baseView.backgroundColor = Constants.appGreenColor
+        
+        view.addSubview(baseView)
+        
+        baseView.translatesAutoresizingMaskIntoConstraints = false
+        baseView.rightAnchor.constraintEqualToAnchor(view.rightAnchor, constant: 0).active = true
+        baseView.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor).active = true
+        baseView.widthAnchor.constraintEqualToConstant(view.frame.width).active = true
+    }
+    
     func createInputBaseField() {
         baseField = UITextField()
         
-        baseField.backgroundColor = UIColor.greenColor()
+        baseField.backgroundColor = Constants.appGreenColor
+        baseField.textColor = UIColor.whiteColor()
+        baseField.font = UIFont(name: "Futura", size: 30.0)
+        baseField.textAlignment = NSTextAlignment.Right
         
         baseField.addTarget(self, action: #selector(changeTip), forControlEvents: .EditingChanged)
         
@@ -36,15 +58,14 @@ class ViewController: UIViewController {
         
         baseField.placeholder = "$"
         
-        baseField.textAlignment = NSTextAlignment.Right
-        
-        view.addSubview(baseField)
+        baseView.addSubview(baseField)
         
         baseField.translatesAutoresizingMaskIntoConstraints = false
-        baseField.rightAnchor.constraintEqualToAnchor(view.rightAnchor, constant: 0).active = true
-        baseField.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor).active = true
-        baseField.widthAnchor.constraintEqualToConstant(view.frame.width/2).active = true
-        baseField.heightAnchor.constraintEqualToConstant(50).active = true
+        baseField.rightAnchor.constraintEqualToAnchor(baseView.rightAnchor, constant: 0).active = true
+        baseField.centerXAnchor.constraintEqualToAnchor(baseView.centerXAnchor).active = true
+        baseField.centerYAnchor.constraintEqualToAnchor(baseView.centerYAnchor).active = true
+        baseField.widthAnchor.constraintEqualToConstant(baseView.frame.width).active = true
+        baseField.heightAnchor.constraintEqualToConstant(60)
         
         baseField.becomeFirstResponder()
     }
@@ -57,7 +78,7 @@ class ViewController: UIViewController {
         view.addSubview(tipLabel)
         
         tipLabel.translatesAutoresizingMaskIntoConstraints = false
-        tipLabel.topAnchor.constraintEqualToAnchor(baseField.bottomAnchor, constant: 50).active = true
+        tipLabel.topAnchor.constraintEqualToAnchor(baseView.bottomAnchor, constant: 50).active = true
         tipLabel.widthAnchor.constraintEqualToConstant(view.frame.width/2).active = true
         tipLabel.heightAnchor.constraintEqualToConstant(50).active = true
         tipLabel.rightAnchor.constraintEqualToAnchor(view.rightAnchor, constant: 0).active = true
@@ -94,6 +115,17 @@ class ViewController: UIViewController {
     }
     
     func changeTip() {
+        let viewableArea = view.frame.height - keyboardHeight
+        if baseField.text != "" {
+            baseViewConstraint.constant = viewableArea/2
+        } else {
+            baseViewConstraint.constant = viewableArea
+        }
+        
+        UIView.animateWithDuration(0.25) {
+            self.view.layoutIfNeeded()
+        }
+        
         let base = Double(baseField.text!) ?? 0
         
         let tipInts = [0.15, 0.20, 0.25]
@@ -105,6 +137,16 @@ class ViewController: UIViewController {
         
         tipLabel.text = String.localizedStringWithFormat("$%.2f", tip)
         totalLabel.text = String.localizedStringWithFormat("$%.2f", total)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        let frame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        
+        keyboardHeight = frame.height
+        
+        baseViewConstraint = baseView.heightAnchor.constraintEqualToConstant(view.frame.height - keyboardHeight)
+        baseViewConstraint.identifier = "fieldHeight"
+        baseViewConstraint.active = true
     }
 
     override func didReceiveMemoryWarning() {
